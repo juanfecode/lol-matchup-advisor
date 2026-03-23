d365a89fe13b84ad73649ff996aa00bd27b544e2
juanfecode <soyjuanfe@gmail.com>
2026-03-23 17:59:20 -0300

    Add hideAlly Stimulus method and matchup form

--- app/javascript/controllers/matchup_controller.js
+++ app/javascript/controllers/matchup_controller.js
@@ -4,4 +4,13 @@ export default class extends Controller {
   connect() {
     console.log("Matchup controller conectado!")
   }
+
+  hideAlly(event){
+    this.element.querySelectorAll("[data-role]").forEach(element => {
+      element.classList.remove("d-none")
+      if (element.dataset.role == event.target.value) {
+        element.classList.add("d-none")
+      }
+    });
+  }
 }
